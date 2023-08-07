c8156c42c21012a143510134934eeb14b454b579
AmoMonnanyana <amogelangmonnanyana@gmail.com>
2023-08-07 04:45:51 +0200

    apis added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import * as sqlite from 'sqlite';
 import sqlite3 from 'sqlite3';
+import  totalPhoneBill from './totalPhoneBill.js';
+
+const app = express();
+//app.use(express.static('public'))
+app.use(express.json())
 
 const  db = await sqlite.open({
     filename:  './data_plan.db',
@@ -40,9 +45,74 @@ async function deletePricePlan(plan_name){
 
 const result1 = await getPricePlans()
 console.log(result1)
-const result2 = await getSpecificPlan('amo_plan')
+const result2 = await getSpecificPlan('call 201')
 //await updatePlan(2.5, 3.49, 'call 201')
 //await deletePricePlan('amo_plan')
 //await addPricePlan('amo_plan', 1.49, 2.15)
 console.log(result2)
 console.log('end')
+
+console.log(result2[0].plan_name)
+/*app.get('/api/phonebill', async function(req, res){
+const plan_name = req.query.plan_name
+const plan = await getSpecificPlan(plan_name)
+
+res.json({
+plan
+
+})
+})*/
+
+
+app.post('/api/phonebill', async function(req, res){
+    const price_plan = req.body.price_plan
+    const actions = req.body.actions
+
+    const payment_plan = await getSpecificPlan(price_plan)
+    const call_cost = payment_plan[0].call_price
+    const sms_cost = payment_plan[0].sms_price
+
+    const total = totalPhoneBill(actions, call_cost, sms_cost)
+
+    res.json({
+        status: "success",
+        total
+    })
+})
+
+app.get('/api/price_plans/',async  function(req, res){
+    const price_plans = await getPricePlans()
+    res.json({
+        price_plans
+    })
+})
+
+app.post('/api/price_plan/create', async function(req, res){
+    const name = req.body.name
+    const call_cost = req.body.call_cost
+    const sms_cost = req.body.sms_cost
+    await addPricePlan(name, sms_cost, call_cost)
+
+    res.json({
+        status: "success",
+        message: "New price plan added"
+    })
+})
+
+app.post('/api/price_plan/update', async function(req, res){
+    const name = req.body.name
+    const call_cost = req.body.call_cost
+    const sms_cost = req.body.sms_cost
+    await updatePlan(sms_cost, call_cost, name)
+
+    res.json({
+        status: "success",
+        message: "Price plan updated"
+    })
+})
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => `Server started ${PORT}`)
+
